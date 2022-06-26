e6e811380b4a0bceedb07201b7f6a2b9e514e901
Kudriavtsev Aleksandr Sergeevich <lasacc4@yandex.ru>
2022-06-26 19:20:47 +0300

    Готовый девятый юнит.

---
+++ unit9.js
@@ -0,0 +1,35 @@
+// function setScrollBy() {
+// 	window.scrollBy(0, 50);
+// 	const windowScrollTop = window.pageYOffset;
+// 	console.log(windowScrollTop);
+// }
+
+// function setScrollTo(){
+// 	window.scrollTo(0, 50);
+// 	const windowScrollTop = window.pageYOffset;
+// 	console.log(windowScrollTop);
+// }
+
+
+// function setScrollToOptions(){
+// 	window.scrollTo({
+// 		top: 500,
+// 		left: 0,
+// 		behavior: "smooth"//instant/auto
+// 	});
+// }
+
+
+// const windowWidth = window.innerWidth;
+
+// const mainElement = document.documentElement;
+// const mainElementWidth = mainElement.clientWidth;
+
+// console.log(mainElementWidth);
+
+
+// const item =  document.querySelector('.cords__title');
+// const getItemCords = item.getBoundingClientRect();
+// console.log(getItemCords);
+
+// setTimeout ('window.scrollBy(0, 100)', 1 * 1000);
