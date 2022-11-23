9aed5a60a2848c10b6b17520552a2c0dbaa92b4c
Aleks <lasacc4@yandex.ru>
2022-11-23 19:43:25 +0300

    bs in rotated arr

---
+++ leetcode/searchInRotated.js
@@ -0,0 +1,26 @@
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while(left <= right) {
+        const middle = Math.floor((left + right) / 2);
+        
+        if (nums[middle] === target) return middle;
+        
+        if (nums[left] <= nums[middle]) {
+            if (target < nums[left] || target > nums[middle]) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        } else{
+            if (nums[middle] > target || nums[right] < target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1
+            } 
+        }
+    
+    }
+    return - 1;
+};
